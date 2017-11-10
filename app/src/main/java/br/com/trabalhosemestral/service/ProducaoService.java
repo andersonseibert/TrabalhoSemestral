88276a9fe2b79c5ac42e9d4f27cbb6704ef01e44
Anderson Seibert <anderson_seibert@hotmail.com>
2017-11-10 21:32:16 -0200

    No Commit Message

---
+++ app/src/main/java/br/com/trabalhosemestral/service/ProducaoService.java
@@ -0,0 +1,34 @@
+package br.com.trabalhosemestral.service;
+
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import br.com.trabalhosemestral.model.Producao;
+
+
+public class ProducaoService {
+
+    public void Inserir(Producao producao){
+
+        JSONObject object = new JSONObject();
+
+    }
+
+    public void Alterar(Producao producao){
+
+    }
+
+    public void Excluir(int codigoProducao){
+
+    }
+
+    public void Buscar(int codigoProducao){
+
+    }
+
+    public List<Producao> ListarTodos(){
+        return null;
+    }
+}
---
+++ app/src/main/java/br/com/trabalhosemestral/service/ProdutoService.java
@@ -0,0 +1,35 @@
+package br.com.trabalhosemestral.service;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import br.com.trabalhosemestral.model.Producao;
+import br.com.trabalhosemestral.model.Produto;
+
+public class ProdutoService {
+
+    public void Inserir(Produto produto){
+
+        JSONObject object = new JSONObject();
+
+    }
+
+    public void Alterar(Produto produto){
+
+    }
+
+    public void Excluir(int codigoProduto){
+
+    }
+
+    public void Buscar(int codigoProduto){
+
+    }
+
+    public List<Produto> ListarTodos(){
+        return null;
+    }
+}
